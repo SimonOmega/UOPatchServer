20a3a162f6fd740483f1f9163d521ed14e62e581
SimonOmega <simonomega@gmail.com>
2015-03-02 04:24:33 -0500

    Cleanup
    Clean Threads and Socket.  Removed usless variables.

--- UOPatchServer.py
+++ UOPatchServer.py
@@ -1,5 +1,27 @@
-'''
-Created on Jul 07, 2013
+#! /usr/bin/env python
+'''Classes that allow the implementation of an UltimaOnline style RTP Patch 
+Server.
+
+Implemented classes:
+  PatchCatalog
+  PatchHandler
+
+PatchCatalog creates and returns a multidimensional Dictionary in the 
+following form:
+{'Win32_UOSA': {34: '\\archive\\uosa_win32_7-0-11-0', 
+                35: '\\archive\\uosa_win32_7-0-11-1', 
+                36: '\\archive\\uosa_win32_7-0-11-2',}}
+Which the data represents:
+{'Client Name': {Patch Release Version: 'Path/File Name (no extention)', ... }}
+
+PatchHandler takes the folloing items:
+  a Connection from an accepted connection
+  an IP Address from and accepted connection
+  a Patch Catalog
+All connections are from a socket of type socket(AF_INET, SOCK_STREAM).
+Patch Handler listens for Client requests and responds appropriately.
+
+Started on Jul 07, 2013
 @author: simonomega
 '''
 
@@ -10,8 +32,12 @@ from struct import pack, unpack
 
 class PatchHandler:
   '''
-  This is a move to a class based structure to support the UO patch server.
-  I'm not a huge fan of classes, but it will help development forward.
+  PatchHandler takes the folloing items:
+    a Connection from an accepted connection
+    an IP Address from and accepted connection
+    a Patch Catalog
+  All connections are from a socket of type socket(AF_INET, SOCK_STREAM).
+  Patch Handler listens for Client requests and responds appropriately.
   '''
 
   def __init__(self, conn, add, catalog):
@@ -53,14 +79,13 @@ class PatchHandler:
       self.connection.send(b'\x00\x00\x00\x01')
 
   def command_patch_list_data(self):
-    print("PATCH LIST BABY")
-    # TODO: I think the Client Expects just one pat and one rtp file.  Not all of them.
     requested_version = (self.reported_version + 1)
     while requested_version in self.patch_catalog.getCatalog()[self.reported_client]:
-    # for a_file in self.patch_catalog.getCatalog():
-      p_file = '.' + self.patch_catalog.getCatalog()[self.reported_client][requested_version] + '.pat'
+      p_file = path.join(self.patch_catalog.getCatalog()['path'],
+        self.patch_catalog.getCatalog()[self.reported_client][requested_version] + '.pat')
       p_head, p_tail = path.split(p_file)
-      r_file = '.' + self.patch_catalog.getCatalog()[self.reported_client][requested_version] + '.rtp'
+      r_file = path.join(self.patch_catalog.getCatalog()['path'],
+        self.patch_catalog.getCatalog()[self.reported_client][requested_version] + '.rtp')
       r_head, r_tail = path.split(r_file)
       file_listing = {'patname':p_tail.encode('ascii'),
                       'patnamelen':pack('>i', 
@@ -84,24 +109,26 @@ class PatchHandler:
     self.connection.send(b'\x00\x00\x00\x00')
 
   def command_send_patch(self, target_file = None):
-      if target_file == None:
-        print("\nTrying to send PatchData to Client, but no target_file name was sent to command 4.\n")
-      else:
-        file_properties = {'working_filename':target_file,
-                           'namelen':pack('>i', len(target_file.encode('ascii'))),
-                           'working_filelen':path.getsize(path.join(self.working_directory, target_file))}
-        print("\nSERVER << sending %s PatchData for %s\n" % (self.address, file_properties))
-        self.connection.send(file_properties['namelen'])
-        self.connection.send(file_properties['working_filename'].encode('ascii'))
-        self.connection.send(pack('>i', file_properties['working_filelen']))
-        with open(path.join(self.working_directory, file_properties['working_filename']), 'rb') as push_file:
+    if target_file == None:
+      print("\nTrying to send PatchData to Client, but no target_file name was sent to command 4.\n")
+      self.command_done_transfer()
+    else:
+      self.patch_catalog.getCatalog()[self.reported_client]
+      file_properties = {'working_filename':target_file,
+                         'namelen':pack('>i', len(target_file.encode('ascii'))),
+                         'working_filelen':path.getsize(path.join(self.working_directory, target_file))}
+      print("\nSERVER << sending %s PatchData for %s\n" % (self.address, file_properties))
+      self.connection.send(file_properties['namelen'])
+      self.connection.send(file_properties['working_filename'].encode('ascii'))
+      self.connection.send(pack('>i', file_properties['working_filelen']))
+      with open(path.join(self.working_directory, file_properties['working_filename']), 'rb') as push_file:
+        block = push_file.read(1024)
+        while block:
+          self.connection.send(pack('>i', len(block)))
+          self.connection.send(block)
           block = push_file.read(1024)
-          while block:
-            self.connection.send(pack('>i', len(block)))
-            self.connection.send(block)
-            block = push_file.read(1024)
-        print(r"SERVER << done sending %s PatchData for %s sending \x00\x00\x00\x00 Block Size" % (self.address, file_properties))
-        self.connection.send(pack('>i', 0))
+      print(r"SERVER << done sending %s PatchData for %s sending \x00\x00\x00\x00 Block Size" % (self.address, file_properties))
+      self.connection.send(pack('>i', 0))
 
   def command_done_transfer(self):
     self.connection.send(b'\x00\x00\x00\x00')
@@ -170,18 +197,27 @@ class PatchHandler:
 
 class PatchCatalog:
   '''
+  PatchCatalog creates and returns a multidimensional Dictionary in the 
+  following form:
+  {'Win32_UOSA': {34: '\\archive\\uosa_win32_7-0-11-0', 
+                  35: '\\archive\\uosa_win32_7-0-11-1', 
+                  36: '\\archive\\uosa_win32_7-0-11-2',}}
+  Which the data represents:
+  {'Client Name': {Patch Release Version: 'Path/File Name (no extention)', 
+                   ... }}
   '''
   def __init__(self, directory):
     self.catalog = {}
+    self.catalog['path'] = directory
     for a_file in listdir(directory):
       if a_file.endswith(".pat"):
         with open(path.join(directory, a_file)) as patch_info:
           client, version = next(patch_info).rstrip().split(' ')
           if client in self.catalog:
-            self.catalog[client][int(version)] = path.join(directory, a_file).split('.')[1]
+            self.catalog[client][int(version)] = a_file.split('.')[0]
           else:
             self.catalog[client] = {}
-            self.catalog[client][int(version)] = path.join(directory, a_file).split('.')[1]
+            self.catalog[client][int(version)] = a_file.split('.')[0]
     print("Patch Catalog\n%s\n" % self.catalog)
 
   def getCatalog(self):
@@ -189,16 +225,10 @@ class PatchCatalog:
 
 if __name__ == "__main__":
   _PATCH_DIR = path.join('.', 'archive')
-  # _HOST = '127.0.0.1' # Loopback
-  # _HOST = ''# Listen on all IPs
   # _HOST = socket.gethostname()
-  _HOST = 'localhost'
-  # docs.python.org - local host - On most platforms, this will take a shortcut around a couple of layers of network code and be quite a bit faster.'''
+  _HOST = 'localhost'  # https://docs.python.org/2/howto/sockets.html#ipc
   _PORT = 8888
   _RUNNING = True
-  # _PATCHES = {}
-  _CLIENTS = {}
-  _THREADS = []
   
   print("Generating Patch Catalog\n")
   _UO_CATALOG = PatchCatalog(_PATCH_DIR)
@@ -206,13 +236,12 @@ if __name__ == "__main__":
   opensocket = socket(AF_INET, SOCK_STREAM)
   opensocket.bind((_HOST, _PORT))
   print("%s listening on %i\n" % (_HOST, _PORT))
-  # opensocket.listen(1) # queue up to 1 connection.
-  opensocket.listen(5) # queue up to 5 connections.
+  opensocket.listen(5)  # queue up to 5 connections.
   while _RUNNING:
     print("Socket: Waiting on Connections.\n")
     connection, ip_port = opensocket.accept()
-    _CLIENTS[ip_port[0]] = PatchHandler(connection, ip_port[0], _UO_CATALOG)
-  #  _CLIENTS[ip_port[0]] = Thread(target = PatchListener, args = (connection, address[0]))
-    _THREADS.append(Thread(target = _CLIENTS[ip_port[0]].run).start())
-  #  _CLIENTS[ip_port[0]].start()
+    #_THREADS.append(Thread(target = PatchHandler(connection, 
+    Thread(target = PatchHandler(connection,
+                                 ip_port[0], _UO_CATALOG).run).start()
+    # )
   print("MAIN _RUNNING == False")
