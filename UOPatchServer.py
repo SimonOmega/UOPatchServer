ac6274a599cf9e573091daf8edd0bf5c1763bbcf
SimonOmega <simonomega@gmail.com>
2015-03-02 13:19:29 -0500

    Functional Version For Beta Testing
    Functional Version For Beta Testing

--- UOPatchServer.py
+++ UOPatchServer.py
@@ -47,7 +47,7 @@ class PatchHandler:
     self.address = add
     self.reported_version = -1
     self.reported_client = 'NONE'
-    # self.file_list = patches
+    self.file_request = None
     self.patch_catalog = catalog
 
   def command_server_trasnfer(self):
@@ -107,21 +107,23 @@ class PatchHandler:
       print("\nSERVER << sent %s PatchListData entry %s\n" % (self.address, file_listing))
       requested_version = requested_version + 1
     self.connection.send(b'\x00\x00\x00\x00')
+    return
 
-  def command_send_patch(self, target_file = None):
-    if target_file == None:
+  def command_send_patch(self):
+    if self.file_request == None or self.file_request['filename'] == None:
       print("\nTrying to send PatchData to Client, but no target_file name was sent to command 4.\n")
       self.command_done_transfer()
     else:
-      self.patch_catalog.getCatalog()[self.reported_client]
-      file_properties = {'working_filename':target_file,
-                         'namelen':pack('>i', len(target_file.encode('ascii'))),
-                         'working_filelen':path.getsize(path.join(self.working_directory, target_file))}
+      send_file = path.join(self.patch_catalog.getCatalog()['path'], 
+                            self.file_request['filename'])
+      file_properties = {'filename':self.file_request['filename'],
+                         'filenamelen':pack('>i', len(self.file_request['filename'].encode('ascii'))),
+                         'filesize':path.getsize(send_file)}
       print("\nSERVER << sending %s PatchData for %s\n" % (self.address, file_properties))
-      self.connection.send(file_properties['namelen'])
-      self.connection.send(file_properties['working_filename'].encode('ascii'))
-      self.connection.send(pack('>i', file_properties['working_filelen']))
-      with open(path.join(self.working_directory, file_properties['working_filename']), 'rb') as push_file:
+      self.connection.send(file_properties['filenamelen'])
+      self.connection.send(file_properties['filename'].encode('ascii'))
+      self.connection.send(pack('>i', file_properties['filesize']))
+      with open(send_file, 'rb') as push_file:
         block = push_file.read(1024)
         while block:
           self.connection.send(pack('>i', len(block)))
@@ -129,6 +131,8 @@ class PatchHandler:
           block = push_file.read(1024)
       print(r"SERVER << done sending %s PatchData for %s sending \x00\x00\x00\x00 Block Size" % (self.address, file_properties))
       self.connection.send(pack('>i', 0))
+    self.file_request = {'filenamelen':0, 'filename':None, 'padding':0}
+    return
 
   def command_done_transfer(self):
     self.connection.send(b'\x00\x00\x00\x00')
@@ -137,48 +141,38 @@ class PatchHandler:
     self.data = self.connection.recv(self.buffer_len)
     if self.data == b'\x00\x00\x00\x15':
       print("\nCLIENT >> %s sent Client Hello/Request (%s)\n" % (self.address, self.data))
-      # self.command_notification() # Notificaions force a disconnect...
-      # self.command_notification("Welcome to the patch server.") # Notification here causes Client Protocol Error
       Thread(target = self.command_use_self).start()
     elif self.data == b'\x00\x00\x00\x01':
-      # version_info = {'namelen':0, 'client':"", 'version':0}
       self.data = self.connection.recv(4)
       namelen = unpack('>L', self.data)[0]
-      # Length of client Name
       self.data = self.connection.recv(namelen)
       self.reported_client = self.data.decode('ascii')
-      # Name of the client Running
       self.data = self.connection.recv(4)
       self.reported_version = unpack('>L', self.data)[0]
-      # Version Number (Patch Number) in this client
       print("\nCLIENT >> %s sent Version Information %s %s\n" % (self.address, self.reported_client, self.reported_version))
       # TODO: Only send a new list if the version has changed.
       if (self.reported_client not in self.patch_catalog.getCatalog()) or ((self.reported_version + 1) not in self.patch_catalog.getCatalog()[self.reported_client]):
         print("\nCLIENT ||  All Patched UP, No new patches.\n")
-        # self.command_notification("You are all patched up.") # Notifications here force a disconnect.
         self.listen = False
         self.connection.send(b'\x00\x00\x00\x00')
         return
       else:
         Thread(target = self.command_patch_list_data).start()
     elif self.data == b'\x00\x00\x00\x02':
-      file_request = {'namelen':0, 'working_filename':'', 'padding':0}
+      self.file_request = {'filenamelen':0, 'filename':None, 'padding':0}
       self.data = self.connection.recv(4)
-      file_request['namelen'] = unpack('>L', self.data)[0]
-      self.data = self.connection.recv(file_request['namelen'])
-      file_request['working_filename'] = self.data.decode('ascii')
+      self.file_request['filenamelen'] = unpack('>L', self.data)[0]
+      self.data = self.connection.recv(self.file_request['filenamelen'])
+      self.file_request['filename'] = self.data.decode('ascii')
       self.data = self.connection.recv(4)
-      file_request['padding'] = unpack('>L', self.data)[0]
+      self.file_request['padding'] = unpack('>L', self.data)[0]
       # Always Null Assuming Unused... Or used to continue a failed download...
-      print("\nCLIENT >> %s sent a Request for File %s (%s)\n" % (self.address, file_request['working_filename'], file_request))
-      # I think padding is used for resuming a file from the last fragment that downloaded.
-      ###
-      # TODO: Ensure send_patch supports the new catalog structure.
-      ###
-      # Thread(target = self.command_send_patch, args = (4, file_request['working_filename'])).start()
+      print("\nCLIENT >> %s sent a Request for File %s (%s)\n" % (self.address,
+            self.file_request['filename'], 
+            self.file_request))
+      Thread(target = self.command_send_patch).start()
     elif self.data == b'\x00\x00\x00\x03':
       Thread(target = self.command_done_transfer).start()
-      # TODO: Investigate if we should have a latest version check here.
     else:
       print("\nCLIENT >> %s sent unhandled message %s\n" % (self.address, self.data)) 
       # TODO: Handle Error when Client Disconnects.
